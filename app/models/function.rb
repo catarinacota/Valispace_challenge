171b888a43a19e4bcec9c9d90ed66fc7503c458f
catarinacota <catarina.afc@gmail.com>
2018-07-21 23:18:46 +0100

    home page layout; model function name format

--- app/models/function.rb
+++ app/models/function.rb
@@ -1,7 +1,8 @@
 class Function < ApplicationRecord
   belongs_to :user
 
-  validates :name, presence: true, uniqueness: true
+  validates :name, presence: true, uniqueness: true, format: { with: /\A[\w-]+\z/,
+    message: "invalid format. ex: f_1 / f1 / f-1" }
   validates :content, presence: true
 
 end
